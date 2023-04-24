1d78f4605e3276bb1ba63391aa63e2a4699d7a98
arivusurya <arivusurya.3@gmail.com>
2023-04-24 22:50:18 +0530

    Part time salary for an emplayee

--- Employee.cs
+++ Employee.cs
@@ -5,20 +5,27 @@ namespace employee_payroll_oops
 
         public static int wagePerHour = 20;
         public  static int FulldayHour = 8;
+
+        public static int ParttimeHour = 4;
+
         public static int  checkEmployee(){
             Random random = new Random();
-            int employeecheck = random.Next(0,2);
+            int employeecheck = random.Next(0,3);
             return employeecheck;
         }
 
         public static void dailywage(){
+            int dailywage = 0;
             int emplyeecheck = checkEmployee();
             if(emplyeecheck == 1){
-                int dailywage = wagePerHour * FulldayHour ;
+                 dailywage = wagePerHour * FulldayHour ;
                 Console.WriteLine("Dailywage of Employee: " + dailywage);
-            }
-
-           
+            }else if(emplyeecheck == 2){
+                 dailywage = wagePerHour * ParttimeHour ;
+                Console.WriteLine("Dailywage of Employee: " + dailywage);
+            }else{
+                Console.WriteLine("Dailywage of Employee: " + dailywage);
+            }           
          }
 
     }
